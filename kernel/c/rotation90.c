a272dc24e69d14f7eb0fe64ab148695fd4c03d08
charles66820 <charles.goedefroit@gmail.com>
2022-03-02 01:10:12 +0100

    omp rotation

--- kernel/c/rotation90.c
+++ kernel/c/rotation90.c
@@ -29,3 +29,58 @@ unsigned rotation90_compute_seq (unsigned nb_iter)
 
   return 0;
 }
+
+///////////////////////////// Simple parallale version (omp)
+// Suggested cmdline:
+// ./run -l images/shibuya.png -k rotation90 -v omp
+//
+unsigned rotation90_compute_omp (unsigned nb_iter)
+{
+  for (unsigned it = 1; it <= nb_iter; it++) {
+
+    #pragma omp parallel
+    do_tile (0, 0, DIM, DIM, omp_get_thread_num());
+
+    swap_images ();
+  }
+
+  return 0;
+}
+
+
+///////////////////////////// Simple sequential version (tiled)
+// Suggested cmdline:
+// ./run -l images/shibuya.png -k rotation90 -v tiled --pause
+//
+unsigned rotation90_compute_tiled (unsigned nb_iter)
+{
+  for (unsigned it = 1; it <= nb_iter; it++) {
+
+    for (int y = 0; y < DIM; y += TILE_H)
+      for (int x = 0; x < DIM; x += TILE_W)
+        do_tile (x, y, TILE_W, TILE_H, 0);
+
+    swap_images ();
+  }
+
+  return 0;
+}
+
+///////////////////////////// Simple parallale version (omp_tiled)
+// Suggested cmdline:
+// ./run -l images/shibuya.png -k rotation90 -v omp_tiled
+//
+unsigned rotation90_compute_omp_tiled (unsigned nb_iter)
+{
+  for (unsigned it = 1; it <= nb_iter; it++) {
+
+#pragma omp parallel for schedule(runtime) collapse(2)
+    for (int y = 0; y < DIM; y += TILE_H)
+      for (int x = 0; x < DIM; x += TILE_W)
+        do_tile (x, y, TILE_W, TILE_H, omp_get_thread_num());
+
+    swap_images ();
+  }
+
+  return 0;
+}
